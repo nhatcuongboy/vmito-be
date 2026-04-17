6867731231981ecda733f96b3bfb5484b4e9e5b5
nhatcuongboy <nhatcuongboy@gmail.com>
2026-04-17 16:33:50 +0700

    feat: add description and court layout fields to Venue model and DTOs

--- prisma/migrations/20260417000000_add_venue_amenity_fields/migration.sql
+++ prisma/migrations/20260417000000_add_venue_amenity_fields/migration.sql
@@ -2,10 +2,13 @@
 CREATE TYPE "ClosureStatus" AS ENUM ('OPERATING', 'PERMANENTLY_CLOSED', 'TEMPORARILY_CLOSED');
 
 -- AlterTable
-ALTER TABLE "venues" ADD COLUMN "hasCarParking" BOOLEAN,
+ALTER TABLE "venues" ADD COLUMN "description" TEXT,
+ADD COLUMN "hasCarParking" BOOLEAN,
 ADD COLUMN "hasCanteen" BOOLEAN,
 ADD COLUMN "wifiName" TEXT,
 ADD COLUMN "wifiPassword" TEXT,
 ADD COLUMN "closureStatus" "ClosureStatus" NOT NULL DEFAULT 'OPERATING',
 ADD COLUMN "bookingPolicy" TEXT,
-ADD COLUMN "locatedWithin" TEXT;
+ADD COLUMN "locatedWithin" TEXT,
+ADD COLUMN "courtLayoutImage" TEXT,
+ADD COLUMN "courtLayoutImagePublicId" TEXT;
