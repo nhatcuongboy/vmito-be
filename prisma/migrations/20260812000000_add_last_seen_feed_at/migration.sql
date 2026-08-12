7a2e303845210cc6d0287e922ed2894d2a0d0941
Nhat Cuong <nhatcuongboy@gmail.com>
2026-08-12 19:09:48 +0700

    feat(migrations): add IF NOT EXISTS clause to posts index creation

--- prisma/migrations/20260812000000_add_last_seen_feed_at/migration.sql
+++ prisma/migrations/20260812000000_add_last_seen_feed_at/migration.sql
@@ -2,4 +2,5 @@
 ALTER TABLE "users" ADD COLUMN "lastSeenFeedAt" TIMESTAMP(3);
 
 -- Create index for efficient querying of posts newer than lastSeenFeedAt
-CREATE INDEX "posts_createdAt_idx" ON "posts"("createdAt");
+CREATE INDEX IF NOT EXISTS "posts_createdAt_idx" ON "posts"("createdAt");
+
